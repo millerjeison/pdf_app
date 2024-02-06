5fcca00a80a2623edddddcf2c7429eff260ed708
Jeison Miller <millerjeison@gmail.com>
2024-02-06 08:47:03 -0500

    funciona el subir archivo y agrega los input

--- src/app/pages/edit-pdf/edit-pdf.component.ts
+++ src/app/pages/edit-pdf/edit-pdf.component.ts
@@ -73,6 +73,7 @@ export class EditPdfComponent {
         thumbnailCanvas.width = viewport.width;
         thumbnailCanvas.height = viewport.height;
 
+        
         const thumbnailContext = thumbnailCanvas.getContext('2d') as CanvasRenderingContext2D;
         const renderContext = {
           canvasContext: thumbnailContext,
@@ -192,10 +193,29 @@ export class EditPdfComponent {
       const pdfDocument = await pdfjsLib.getDocument(pdfUrl).promise;
       const firstPage = await pdfDocument.getPage(1);
       const metadata = await pdfDocument.getMetadata();
-      console.log(pdfDocument.getFieldObjects());
 
-      console.log("metadata",metadata);
+      firstPage.getAnnotations().then(annotations => {
+        annotations.forEach(annotation => {
 
+          console.log('annotation',annotation);
+          
+        });
+
+      })
+
+
+      let Keywords = (metadata.info as any).Keywords;
+      // Verificar si 'Keywords' existe y no está vacío
+      if (Keywords&&Keywords.length > 0) {
+        // Parsear el contenido de 'Keywords' que está en formato string JSON
+        const keywordsData = JSON.parse(Keywords);
+        for (const i of keywordsData) {
+          let meta = this.metaDataFromJson(i);
+          const newMetadata: metaData = meta;
+          this.metadata.push(newMetadata);
+          this.inputs = this.metadata.find(i => i.pageNumber === this.paneInit)?.inputs || [];
+        }
+      }
 
       this.viewport = firstPage.getViewport({ scale: 1.5 });
       canvas.width = this.viewport.width;
@@ -217,7 +237,21 @@ export class EditPdfComponent {
       console.error('Error al cargar el PDF:', error);
     }
   }
+  metaDataFromJson(json: any): metaData {
+    // Asegúrate de que tu JSON tiene la estructura correcta y que 'inputs' es un array
+    if (!json || typeof json.pageNumber !== 'number' || !Array.isArray(json.inputs)) {
+      throw new Error('JSON format is invalid for metaData');
+    }
 
+    // Aquí podrías llamar a `Input.fromJson` para cada input si `Input` es una clase con ese método estático
+    const inputs: Input[] = json.inputs.map((inputJson: any) => Input.fromJson(inputJson));
+
+    // Construye el objeto metaData
+    return {
+      pageNumber: json.pageNumber,
+      inputs: inputs,
+    };
+  }
   resizeCanvas(width: number, height: number) {
     const canvas: HTMLCanvasElement = this.pdfCanvas.nativeElement;
     const drawingCanvas: HTMLCanvasElement = this.drawingCanvas.nativeElement;
@@ -308,8 +342,8 @@ export class EditPdfComponent {
       this.selectedInput = null;
     }
   }
-  
-  addInputToCanvas(x: number, y: number, width: number, height: number, inputType?: string) {
+
+  addInputToCanvas(x: number, y: number, width: number, height: number, inputType?: string, pageNumberDefault?: number): void {
     const containInput = document.querySelector('.contain_input');
     if (!containInput) {
       console.error('No se encontró el contenedor con la clase .contain_input');
@@ -348,16 +382,26 @@ export class EditPdfComponent {
     const pageNumber = this.paneInit;
     const metadataIndex = this.metadata.findIndex(item => item.pageNumber === pageNumber);
 
-    if (metadataIndex !== -1) {
-      this.metadata[metadataIndex].inputs.push(rectangleData);
-    } else {
+    if (pageNumberDefault) {
       const newMetadata: metaData = {
-        pageNumber: pageNumber,
+        pageNumber: pageNumberDefault,
         inputs: [rectangleData],
       };
+    } else {
+      if (metadataIndex !== -1) {
+        this.metadata[metadataIndex].inputs.push(rectangleData);
+      } else {
+        const newMetadata: metaData = {
+          pageNumber: pageNumber,
+          inputs: [rectangleData],
+        };
 
-      this.metadata.push(newMetadata);
+        this.metadata.push(newMetadata);
+      }
     }
+
+
+
     this.input = rectangleData;
     this.inputs = this.metadata.find(i => i.pageNumber === this.paneInit)?.inputs || [];
     this.selectRectangle(rectangleData);
@@ -394,22 +438,26 @@ export class EditPdfComponent {
     try {
       const existingPdfBytes = await fetch(this.pdfUrl!).then((res) => res.arrayBuffer());
       const pdfDoc = await PDFDocument.load(existingPdfBytes);
-  
+
       // Asegúrate de que los metadatos están en el formato correcto, como un string JSON.
       const metadataString = JSON.stringify(this.metadata);
-  
+
       // Aquí puedes añadir los metadatos como un campo de texto personalizado o como metadatos en el documento.
       // Es importante que no interfieran con la visualización del documento.
       pdfDoc.setTitle(this.fileName + "_modified.pdf");
       pdfDoc.setProducer('Your Application Metadata');
       pdfDoc.setCreator('Your Application Name');
       pdfDoc.setKeywords([metadataString]); // Esto es solo un ejemplo, ajusta según sea necesario.
-  
+
       const modifiedPdfBytes = await pdfDoc.save();
       const blob = new Blob([modifiedPdfBytes], { type: 'application/pdf' });
       const link = document.createElement('a');
       link.href = URL.createObjectURL(blob);
-      link.download = this.fileName + "_modified.pdf";
+
+      let newName = this.fileName?.replace('.pdf', '');
+
+
+      link.download = `${newName}_modified.pdf`;
       link.click();
     } catch (error) {
       console.error('Error al procesar el PDF:', error);
@@ -420,8 +468,8 @@ interface metaData {
   pageNumber: number;
   inputs: Input[];
 }
-interface Input {
-  id: number,
+class Input {
+  id: number;
   x: string;
   y: string;
   type: string;
@@ -430,4 +478,43 @@ interface Input {
   label?: string;
   checked: boolean;
   inputId: number;
+
+  constructor(
+    id: number,
+    x: string,
+    y: string,
+    type: string,
+    width: string,
+    height: string,
+    label: string | undefined,
+    checked: boolean,
+    inputId: number
+  ) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.type = type;
+    this.width = width;
+    this.height = height;
+    this.label = label;
+    this.checked = checked;
+    this.inputId = inputId;
+  }
+
+  // Método estático para crear una instancia de Input desde un objeto JSON
+  static fromJson(json: any): Input {
+    // Asumiendo que json es un objeto con las propiedades correctas
+    // Aquí deberías agregar validaciones o lógica de transformación si es necesario
+    return new Input(
+      json.id,
+      json.x,
+      json.y,
+      json.type,
+      json.width,
+      json.height,
+      json.label,
+      json.checked,
+      json.inputId
+    );
+  }
 }
